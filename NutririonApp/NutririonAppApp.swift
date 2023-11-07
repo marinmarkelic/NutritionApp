b9d65087d795d72d1c3f1b19c28a9d1f69244032
Marin Markelić <101728747+marinmarkelic@users.noreply.github.com>
2023-11-07 15:22:22 +0100

    Initial Commit

---
+++ NutririonApp/NutririonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutririonAppApp.swift
+//  NutririonApp
+//
+//  Created by Marin on 07.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct NutririonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
