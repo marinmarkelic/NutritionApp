dcbeefce2462bde1dd401e88d3f8df6d4f729d0a
marinmarkelic <101728747+marinmarkelic@users.noreply.github.com>
2024-05-02 19:27:43 +0200

    added circular graph

--- NutritionApp/Home/HomeView.swift
+++ NutritionApp/Home/HomeView.swift
@@ -106,9 +106,8 @@ struct HomeView: View {
 
     func macrosCard(title: String, currentValue: Float, targetValue: Float) -> some View {
         HStack {
-            Rectangle()
-                .fill(Color.red)
-                .frame(width: 40, height: 40)
+            CircularProgressView(progress: Double(currentValue / targetValue), color: .red)
+                .frame(width: 60, height: 60)
 
             VStack {
                 Text(title)
@@ -139,4 +138,35 @@ struct MealCell: View {
         .maxWidth()
         .background(Color.element)
     }
+
+}
+
+struct CircularProgressView: View {
+
+    let graphProgress: Double
+    let progressText: String
+    let color: Color
+    let lineWidth: CGFloat = 5
+
+    init(progress: Double, color: Color) {
+        self.graphProgress = progress > 1 ? 1 : progress
+        self.progressText = "\(Int(progress * 100))%"
+        self.color = color
+    }
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(color.opacity(0.5), lineWidth: lineWidth)
+
+            Circle()
+                .trim(from: 0, to: graphProgress)
+                .stroke(color, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round))
+                .rotationEffect(.degrees(-90))
+
+            Text(progressText)
+                .font(.callout)
+        }
+    }
+
 }
